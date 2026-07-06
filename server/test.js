0d41a7e69d592a5beddccb4d04289430d2d9f5ce
Ritik Rai <cs.ritikrai@gmail.com>
2026-07-06 22:16:55 +0530

    test.js

--- server/test.js
+++ server/test.js
@@ -15,3 +15,39 @@ try {
 }
 
 process.exit();
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+try {
+  console.log("URI:", process.env.MONGODB);
+
+  const conn = await mongoose.connect(process.env.MONGODB);
+
+  console.log("Connected!");
+  console.log(conn.connection.host);
+} catch (err) {
+  console.error(err);
+}
+
+process.exit();
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+try {
+  console.log("URI:", process.env.MONGODB);
+
+  const conn = await mongoose.connect(process.env.MONGODB);
+
+  console.log("Connected!");
+  console.log(conn.connection.host);
+} catch (err) {
+  console.error(err);
+}
+
+process.exit();
