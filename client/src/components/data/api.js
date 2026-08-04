d74d26dd5e00fd011ce91cc889a24c240313dfe4
Ritik Rai <cs.ritikrai@gmail.com>
2026-08-04 22:26:09 +0530

    guest mode

---
+++ client/src/components/data/api.js
@@ -0,0 +1,191 @@
+const API = import.meta.env.VITE_API_URL;
+
+
+const authHeaders = () => {
+
+    const token = localStorage.getItem("token");
+
+
+    return {
+
+        ...(token && {
+            Authorization: `Bearer ${token}`
+        }),
+
+        "Content-Type": "application/json"
+
+    };
+
+};
+
+
+
+
+
+export async function getDashboard() {
+
+
+    const res = await fetch(
+
+        `${API}/api/dashboard`,
+
+        {
+            headers: authHeaders()
+        }
+
+    );
+
+
+    const data = await res.json();
+
+
+    return data.dashboard;
+
+}
+
+
+
+
+
+
+
+export async function getUser() {
+
+
+    const res = await fetch(
+
+        `${API}/api/users/profile`,
+
+        {
+            headers: authHeaders()
+        }
+
+    );
+
+
+    const data = await res.json();
+
+
+    return data.user;
+
+}
+
+
+
+
+
+
+
+export async function getTransactions(
+    page = 1,
+    limit = 10
+) {
+
+
+    const response = await fetch(
+
+        `${API}/api/transactions?page=${page}&limit=${limit}`,
+
+        {
+
+            headers: authHeaders()
+
+        }
+
+    );
+
+
+    return await response.json();
+
+}
+
+
+
+
+
+
+
+export async function addTransaction(transaction) {
+
+
+    const res = await fetch(
+
+        `${API}/api/transactions`,
+
+        {
+
+            method:"POST",
+
+            headers:authHeaders(),
+
+            body:JSON.stringify(transaction)
+
+        }
+
+    );
+
+
+    return await res.json();
+
+}
+
+
+
+
+
+
+
+export async function updateTransaction(
+    id,
+    transaction
+) {
+
+
+    const res = await fetch(
+
+        `${API}/api/transactions/${id}`,
+
+        {
+
+            method:"PUT",
+
+            headers:authHeaders(),
+
+            body:JSON.stringify(transaction)
+
+        }
+
+    );
+
+
+    return await res.json();
+
+}
+
+
+
+
+
+
+
+export async function deleteTransaction(id) {
+
+
+    const res = await fetch(
+
+        `${API}/api/transactions/${id}`,
+
+        {
+
+            method:"DELETE",
+
+            headers:authHeaders()
+
+        }
+
+    );
+
+
+    return await res.json();
+
+}
